5bf197c36721b1570ad230028e00fdd6ed08fe5c
Ayian-Qaisar <ayianqaisar20@gmail.com>
2023-06-09 15:25:12 +0500

    added noman component

--- team-project/src/App.js
+++ team-project/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Noman from "./Noman";
 
 const App = () => {
-  return <div>App is created by ayan</div>;
+  return (
+    <div>
+      App is created by ayan
+      <Noman />
+    </div>
+  );
 };
 
 export default App;
