0189f3c3f06da42d6baa9b4eebc4cb045f081a67
Mqadeer2 <qadeer0301@gmail.com>
2023-06-09 18:20:57 +0500

    Qadeer component has been made

--- team-project/src/App.js
+++ team-project/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Ayian from "./component/Ayian";
+import Qadeer from "./component/Qadeer";
+import './App.css'
 
 const App = () => {
   return (
     <>
       {/*  Add your components here
+      
        */}
       <Ayian />
+      <Qadeer/>
+
     </>
   );
 };
