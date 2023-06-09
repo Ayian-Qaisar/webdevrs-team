ef36c836c97dbed5b76bad0edfb165edd23979c8
Ayian-Qaisar <ayianqaisar20@gmail.com>
2023-06-09 16:49:32 +0500

    everything is clear start your work

--- team-project/src/App.js
+++ team-project/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>App is created by ayan</div>;
+  return <>{/*  Add your components here  */}</>;
 };
 
 export default App;
--- team-project/src/Zaheer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Zaheer = () => {
-  return (
-    <div>Zaheer is components start</div>
-  )
-}
-
-export default Zaheer
