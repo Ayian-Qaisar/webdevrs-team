5d4cc2033ea7191d132bd13acb06151286117e95
Ayian-Qaisar <ayianqaisar20@gmail.com>
2023-06-09 16:56:01 +0500

    added react-bootstrap

--- team-project/src/App.js
+++ team-project/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Ayian from "./component/Ayian";
 
 const App = () => {
-  return <>{/*  Add your components here  */}</>;
+  return (
+    <>
+      {/*  Add your components here
+       */}
+      <Ayian />
+    </>
+  );
 };
 
 export default App;
---
+++ team-project/src/component/Ayian.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Ayian = () => {
+  return <div>Ayian</div>;
+};
+
+export default Ayian;
