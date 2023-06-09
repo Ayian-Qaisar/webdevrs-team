973b2fecf19bd0bd48ba1f8d551235bd3f33e440
Ayian-Qaisar <ayianqaisar20@gmail.com>
2023-06-09 15:32:44 +0500

    added wd component

--- team-project/src/App.js
+++ team-project/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Noman from "./Noman";
+import Wd from "./Wd";
 
 const App = () => {
   return (
     <div>
       App is created by ayan
       <Noman />
-
+      <Wd />
     </div>
   );
 };
---
+++ team-project/src/Wd.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Wd = () => {
+  return <div>Welcome to webdevrs</div>;
+};
+
+export default Wd;
