58f7af15fcd6531dfd4e82b6c2b83a102c6bcb5a
Ayian-Qaisar <ayianqaisar20@gmail.com>
2023-06-09 15:28:08 +0500

    FDSF

--- team-project/src/App.js
+++ team-project/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Noman from "./Noman";
 
 const App = () => {
-  return <div>App is created by ayan</div>;
+  return (
+    <div>
+      App is created by ayan
+      <Noman />
+      git
+    </div>
+  );
 };
 
 export default App;
