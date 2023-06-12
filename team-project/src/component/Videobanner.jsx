67e73960fb73fd0b5172a617559bf8dfeb294806
sammatt777 <134603552+sammatt777@users.noreply.github.com>
2023-06-12 15:17:20 +0500

    Update Videobanner.jsx

--- team-project/src/component/Videobanner.jsx
+++ team-project/src/component/Videobanner.jsx
@@ -6,7 +6,7 @@ const Videobanner = () => {
   return (
     <div>
       
-        <div className='main' style={{position:'relative'}}>
+        <div className='main border' style={{position:'relative'}}>
         <video className='main' style={{width:'100%' ,objectFit:'cover' ,}}  loop  autoPlay muted src="https://cdn.shopify.com/s/files/1/0276/9790/5724/files/bike.mp4?v=1608988545">
         </video>
           <div style={{ width:'100%' ,height:'100%' , position:'absolute' ,bottom:'50px' ,left:'30px' ,top:'50%'}}>
